b30b8acf071aad5a8c26f65ff8591a62970cec65
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2025-02-01 13:15:59 -0800

    caesarSipher(str, shift)
    Take str and shift letters in alphabet, returns string.

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ export const calculator = {
   multiply: (num1, num2) => num1 * num2,
   divide: (num1, num2) => num1 / num2,
 };
+
+export function caesarCipher(str, shift) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const strSplit = str.split('');
+
+  const mapped = strSplit.map((char) => {
+    if (!alphabet.includes(char.toLowerCase())) return char;
+    const charIdx = alphabet.indexOf(char.toLowerCase());
+    const newLetter = alphabet[(charIdx + shift) % alphabet.length];
+    return char === char.toLowerCase() ? newLetter : newLetter.toUpperCase();
+  });
+
+  return mapped.join('');
+}
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from './main';
+import { capitalize, reverseString, calculator, caesarCipher } from './main';
 
 test('first character capitalized', () => {
   expect(capitalize('abc')).toBe('Abc');
@@ -18,4 +18,11 @@ test('add, subtract, multiply, divide', () => {
   expect(calculator.subtract(1, 2)).toBe(-1);
   expect(calculator.multiply(3, 4)).toBe(12);
   expect(calculator.divide(3, 4)).toBe(0.75);
+});
+
+test('string shifted - caesarCipher', () => {
+  expect(caesarCipher('asdfasdf', 1)).toBe('btegbteg');
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 });
